7d69475edc6e2189dfcc3bd07a74bd4b1959e923
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-10 00:31:21 +0700

    a
    a

--- handle_login.php
+++ handle_login.php
@@ -1,15 +1,18 @@
 <?php
 session_start();
 include ('database.php');
-$username = $_GET['username'];
-$password = $_GET['password']; 
+$username = $_POST['username'];
+$password = $_POST['password']; 
 $pg = "SELECT * FROM user WHERE name = '".$username."' AND password ='".$password."'";
+
 
 $result = pg_query($conn, $pg);
 // var_dump($result);
 //die();
 $count = pg_num_rows($result);
-$row = pg_fetch_assoc($result);// w3
+
+echo('".$count."');
+/* $row = pg_fetch_assoc($result);// w3
 
 if($count == 1){
 
@@ -18,5 +21,5 @@ if($count == 1){
 }
 else{
     echo '<script language="javascript">alert("Password or username incorrect !!! try again!!"); window.location="login.php";</script>';
-}
+} */
 ?>
