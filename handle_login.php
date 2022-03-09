bf81ae1a96b2118d37e4e1158d80023fd49d6433
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-10 01:25:37 +0700

    Update handle_login.php

--- handle_login.php
+++ handle_login.php
@@ -3,9 +3,8 @@ session_start();
 include ('database.php');
 $username = $_POST['username'];
 $password = $_POST['password']; 
-echo '<script language="javascript">alert("Password or username incorrect !!! try again!!"); window.location="login.php";</script>';
 
-/* $sql ="SELECT * FROM user WHERE name ='".$username."' AND password ='".$password."' ";
+$sql ="SELECT * FROM user WHERE name ='".$username."' AND password ='".$password."' ";
 $result = pg_query($conn, $sql);
 
 $count = pg_num_rows($result);
@@ -18,7 +17,7 @@ if($count>0){
     header("location: index.php ");
 }else{
     echo '<script language="javascript">alert("Password or username incorrect !!! try again!!"); window.location="login.php";</script>';
-} */
+}
 
 
 
