41b7354efb22a2b62722b2ee4775648af5f7a6c7
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-10 02:12:15 +0700

    a

--- database.php
+++ database.php
@@ -3,7 +3,7 @@ $servername = "ec2-54-158-232-223.compute-1.amazonaws.com";
 $usernameDB = "asoizwbkhphdio";
 $password = "1c8315ab52b394b1e548ad9faf496e2d4a1e3a59a9bc6bda0d527564a07da84f";
 $db = "d7pp5calcap4mh";
-$uri = 'postgres://asoizwbkhphdio:1c8315ab52b394b1e548ad9faf496e2d4a1e3a59a9bc6bda0d527564a07da84f@ec2-54-158-232-223.compute-1.amazonaws com:5432/d7pp5calcap4mh';
+$uri = 'postgres://asoizwbkhphdio:1c8315ab52b394b1e548ad9faf496e2d4a1e3a59a9bc6bda0d527564a07da84f@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d7pp5calcap4mh';
 // Create connection
 $conn = pg_connect($uri);
 
