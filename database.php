fb842181cb43cadd88014bc60bb478c49813d1a4
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-10 00:56:48 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,22 +1,21 @@
 <?php
-/* $servername = "ec2-3-216-221-31.compute-1.amazonaws.com";
+$servername = "ec2-3-216-221-31.compute-1.amazonaws.com";
 $usernameDB = "anmvhqifhrelof";
 $password = "b1c5b9de4ada863333da4e0df19ad62adb8ece194da1d01fb74075ef19b6da04";
 $db = "dr4viqhane5dt";
 $uri = 'postgres://anmvhqifhrelof:b1c5b9de4ada863333da4e0df19ad62adb8ece194da1d01fb74075ef19b6da04@ec2-3-216-221-31.compute-1.amazonaws.com:5432/dr4viqhane5dt';
 // Create connection
-$conn = pg_connect($uri); */
+$conn = pg_connect($uri);
 
-$conn_string = "ec2-3-216-221-31.compute-1.amazonaws.com
-"
-        . " port=5432 dbname=dr4viqhane5dt"
-        . " user=anmvhqifhrelof
-"
-        . " password=b1c5b9de4ada863333da4e0df19ad62adb8ece194da1d01fb74075ef19b6da04";
-$conn = pg_connect($conn_string);
+/* $conn_string = "ec2-3-216-221-31.compute-1.amazonaws.com";
+         " port=5432 dbname=dr4viqhane5dt";
+         " user=anmvhqifhrelof";
+         " password=b1c5b9de4ada863333da4e0df19ad62adb8ece194da1d01fb74075ef19b6da04";
+$conn = pg_connect($conn_string); */
 
 // Check connection
 if (!$conn) {
   die('Error: Could not connect: ' . pg_last_error());
 }
 ?>
+
