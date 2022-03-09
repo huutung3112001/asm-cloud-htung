ed574f8784426664eeb7a4f6ca8dc24cbf867e5d
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-09 23:41:09 +0700

    commit

--- handle_login.php
+++ handle_login.php
@@ -11,7 +11,7 @@ var_dump($result);
 $count = pg_num_rows($result);
 $row = pg_fetch_assoc($result);// w3
 
-if($row == 0){
+if($count == 0){
     echo '<script language="javascript">alert("Password or username incorrect !!! try again!!"); window.location="login.php";</script>';
 
 }
