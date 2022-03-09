f76e35091a9a5e729e440429fc79fbcc9844f48e
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-10 01:12:40 +0700

    Update handle_login.php

--- handle_login.php
+++ handle_login.php
@@ -3,16 +3,35 @@ session_start();
 include ('database.php');
 $username = $_POST['username'];
 $password = $_POST['password']; 
-$pg = "SELECT * FROM user WHERE name = '".$username."' AND password ='".$password."'";
+
+$sql ="SELECT * FROM user WHERE name ='".$username."' AND password ='".$password."' ";
+$result = pg_query($conn, $sql);
+
+$count = pg_num_rows($result);
+
+$row= pg_fetch_array($result);
+
+
+if($count>0){
+    $_SESSION['login_user']=$row['name'];
+    header("location: index.php ");
+}else{
+    echo '<script language="javascript">alert("Password or username incorrect !!! try again!!"); window.location="login.php";</script>';
+}
+
+
+
+
+/* $pg = "SELECT * FROM user WHERE name = '$username' AND password ='$password'";
 
 
 $result = pg_query($conn, $pg);
 // var_dump($result);
-//die();
+session_die();
 $count = pg_num_rows($result);
 
 
-$row = pg_fetch_assoc($result);// w3
+$row = pg_fetch_array($result);// w3
 
 if($count == 1){
 
@@ -21,5 +40,6 @@ if($count == 1){
 }
 else{
     echo '<script language="javascript">alert("Password or username incorrect !!! try again!!"); window.location="login.php";</script>';
-}
+} */
+
 ?>
