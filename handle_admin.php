09da8d666dec521e560478613377e494d82a65ff
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-10 00:02:08 +0700

    c

--- handle_admin.php
+++ handle_admin.php
@@ -3,7 +3,7 @@ session_start();
 include 'database.php';
 $username = $_POST['username'];
 $password = $_POST['password']; 
-$pg = "SELECT * FROM admin where name = '$username' AND password ='$password' ";
+$pg = "SELECT * FROM admin where name = '".$username."' AND password ='".$password."'";
 
 $result = pg_query($conn, $sql);
 // var_dump($result);
