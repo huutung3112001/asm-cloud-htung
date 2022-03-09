1a1f9ab9be439ce284701b10f59d5a5fcf6f6111
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-03-10 00:44:03 +0700

    a
    a

--- database.php
+++ database.php
@@ -7,6 +7,15 @@ $uri = 'postgres://anmvhqifhrelof:b1c5b9de4ada863333da4e0df19ad62adb8ece194da1d0
 // Create connection
 $conn = pg_connect($uri);
 
+// $conn_string = "host=ec2-54-91-188-254.compute-1.amazonaws.com
+// "
+//         . " port=5432 dbname=dcok9uvp4rl5h8"
+//         . " user=dlpyuedphkuxkv
+// "
+//         . " password=28bf6138b9f274a22697fb6f2d531c02f86b95e700ddb2e1bca20935d7ba57c8";
+// $db = pg_connect($conn_string);
+// ec2-54-91-188-254.compute-1.amazonaws.com
+
 // Check connection
 if (!$conn) {
   die('Error: Could not connect: ' . pg_last_error());
